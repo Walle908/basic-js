d8da508f31add2ec6afc85c6d9154e3e4f6e1140
Walle908 <walle0908@yandex.ru>
2025-11-06 23:47:44 +0400

    feat: add solution for count-cats & carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 const MODERN_ACTIVITY = 15;
 const HALF_LIFE_PERIOD = 5730;
@@ -17,11 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== "string") return false;
+  const sample = Number(sampleActivity);
+  if (Number.isNaN(sample) || sample > MODERN_ACTIVITY || sample <= 0)
+    return false;
+
+  const k = Math.LN2 / HALF_LIFE_PERIOD;
+  const t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
+  return t;
 }
 
 module.exports = {
-  dateSample
+  dateSample,
 };
