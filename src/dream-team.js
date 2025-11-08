aea01f77a84575062cb2c21114b2d45336c07c49
Walle908 <walle0908@yandex.ru>
2025-11-08 13:55:37 +0400

    feat: add solution for dream-team & what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Create name of dream team based on the names of its members
@@ -13,11 +13,14 @@ const { NotImplementedError } = require('../lib');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  if (!Array.isArray(members) || members.length === 0) return false;
+  const newArr = members.map((item) => {
+    if (typeof item === "string") return item.trim().slice(0, 1).toUpperCase();
+  });
+  return newArr.sort((a, b) => (a > b ? 1 : -1)).join("");
 }
 
 module.exports = {
-  createDreamTeam
+  createDreamTeam,
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../lib');
+const { NotImplementedError } = require("../lib");
 
 /**
  * Extract season from given date and expose the enemy scout!
@@ -11,11 +11,37 @@ const { NotImplementedError } = require('../lib');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  *
  */
-function getSeason(/* date */) {
-  // Remove line below and write your code here
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  if (arguments.length === 0) return "Unable to determine the time of year!";
+
+  try {
+    date.getUTCMonth();
+  } catch {
+    throw new Error("Invalid date!");
+  }
+
+  switch (date.getUTCMonth()) {
+    case 0:
+    case 1:
+    case 11:
+      return "winter";
+    case 2:
+    case 3:
+    case 4:
+      return "spring";
+
+    case 5:
+    case 6:
+    case 7:
+      return "summer";
+
+    case 8:
+    case 9:
+    case 10:
+      return "autumn";
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
